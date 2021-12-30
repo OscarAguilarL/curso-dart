83243193e75ee7a1a8b5aeee924a89bb75152aa6
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-12-30 11:21:06 -0600

    Herencia en clases

---
+++ ejercicios/06_classes_adv/05_mixins.dart
@@ -0,0 +1,28 @@
+mixin Logger {
+  void imprimir(String texto) {
+    final hoy = DateTime.now();
+    print('$hoy :::: $texto');
+  }
+}
+
+abstract class Astro with Logger {
+  String? nombre;
+
+  Astro() {
+    imprimir('-- Init del Astro --');
+  }
+
+  void existo() {
+    imprimir('-- Soy un ser celestial y existo --');
+  }
+}
+
+// class Asteroide extends Astro {
+//   String nombre;
+
+//   Asteroide(this.nombre);
+// }
+
+main() {
+  // final logger = new Astro();
+}
